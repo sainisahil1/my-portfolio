eddfc8b4826fee774a4bbcfd75e2453e1669d5d6
Sahil <sahils123saini@gmail.com>
2025-04-28 22:04:21 +0200

    name and title

---
+++ app/components/TypingAnimation.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+const TypingAnimation = () => {
+    const text = "Software Engineer | Developer | Problem Solver";
+    const [currentText, setCurrentText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [index, setIndex] = useState(0);
+
+
+    useEffect(() => {
+        const typingSpeed = isDeleting ? 50 : 150;
+        const timeout = setTimeout(() => {
+            if (!isDeleting) {
+              setCurrentText(text.substring(0, index + 1));
+              setIndex(index + 1);
+      
+              if (index === text.length) {
+                setTimeout(() => setIsDeleting(true), 1000); // Pause before deleting
+              }
+            } else {
+              setCurrentText(text.substring(0, index - 1));
+              setIndex(index - 1);
+      
+              if (index === 0) {
+                setIsDeleting(false);
+              }
+            }
+          }, typingSpeed);
+      
+          return () => clearTimeout(timeout);
+    }, [index, isDeleting]);
+
+    return(
+        <div className="text-xl">
+      {currentText}
+      {'|'}
+      </div>
+    );
+
+
+}
+
+
+
+export default TypingAnimation;
